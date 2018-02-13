d8ee8412123bbf69c38b1222a8759fc92d0502ef
Thomas Nicollet <thomas.nicollet@epitech.eu>
2018-02-13 21:14:06 +0100

    Added base material for work Will focus on building a small asembler for now, polishing it next time

---
+++ asm/includes/asm.h
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2017
+** includes/asm.h
+** File description:
+** Assembler header
+*/
+
+#ifndef ASM_H_
+#define ASM_H_
+
+int assembler(int ac, char **av);
+
+#endif /* ASM_H_ */
