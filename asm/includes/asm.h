7ce387c74804c0abf32064c599385e038bc174a6
Thomas Nicollet <thomas.nicollet@epitech.eu>
2018-02-16 15:38:36 +0100

    Encoded perfectly name and comment

--- asm/includes/asm.h
+++ asm/includes/asm.h
@@ -8,6 +8,15 @@
 #ifndef ASM_H_
 #define ASM_H_
 
+void free_it(char **pointer);
+
+#define GARBAGE __attribute__((cleanup(free_it)))
+
+union endianner {
+	int n;
+	char c[4];
+};
+
 int assembler(int ac, char **av);
 
 #endif /* ASM_H_ */
