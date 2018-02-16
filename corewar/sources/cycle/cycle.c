09e8bff6182f0f04f54f30701093e6ac65bcfc09
Alexandre FOURCAT <alexandre.fourcat@epitech.eu>
2018-02-16 10:20:49 +0100

    adding cycle struct of the corewar

---
+++ corewar/sources/cycle/cycle.c
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2017
+** cycle.c
+** File description:
+** cycle handling
+*/
+
+#include "corewar.h"
+
+int exec_pc()
+{
+
+}
+
+int cycle(core_t *core)
+{
+/*
+ * for (int i = 0; i < core->nb_progs; ++i) {
+ * 	exec_pc(core->process_tab[i]);
+ * }
+ * if (!(cycle % cycle_to_die)) {
+ * 	check_alive();
+ * }
+ * core->nbr_cycle++;
+ * 
+*/
+}
