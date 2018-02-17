32a8b5b2774a691827f774771c112ba776d26708
Alexandre FOURCAT <alexandre.fourcat@epitech.eu>
2018-02-17 09:53:12 +0100

    COLORIZED

--- corewar/sources/instructions/instructions_2.c
+++ corewar/sources/instructions/instructions_2.c
@@ -7,9 +7,12 @@
 
 #include "corewar.h"
 #include "instructions.h"
+#include "my_printf.h"
 
 int instruction_error(core_t *core, process_t *process, int *args)
 {
+	my_printf("\e[1;34mGood Job process %s you lost 1 cycle\e[0m\n" \
+			, process->header.prog_name);
 	return(1);
 }
 
