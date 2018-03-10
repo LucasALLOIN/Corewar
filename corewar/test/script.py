468f10aff1d8b48b625011c980610641c9ed1ba0
Thomas Nicollet <thomas.nicollet@epitech.eu>
2018-03-10 20:08:30 +0100

    Added champions for tests

--- corewar/test/script.py
+++ corewar/test/script.py
@@ -1,27 +1,21 @@
+#!/bin/python3
 import os
 
 files = []
-outputs1 = []
-outputs2 = []
-codes = []
-
 for i in os.scandir():
     if i.name[len(i.name) - 2:len(i.name)] == '.s':
         files.append(i.name)
 
+outputs1 = []
+outputs2 = []
 for i in files:
-    outputs1.append(i[0:len(i) - 2] + '.cor')
-    outputs2.append(i[0:len(i) - 2] + '2.cor')
+    outputs1.append(i[0:len(i) - 2] + ".cor")
+    outputs2.append(i[0:len(i) - 2] + "2.cor")
 
-for i,j,k in zip(files,outputs1, outputs2):
-    os.system("../../bonus/binaires-champ-corewar/asm " + i)
+for i,j,k in zip(files, outputs1, outputs2):
+    os.system("../../bonus/binaires-champ-corewar/asm {0}".format(i))
     os.system("mv {0} {1}".format(j, k))
-
-for i,j, k in zip(files, outputs1, outputs2):
-    os.system("../../asm/asm " + i)
-    if os.system("diff {0} {1}".format(j, k)):
-        codes.append(i)
-
-print("Fichiers ne passant pas")
-for i in codes:
-    print(i)
+    os.system("../../asm/asm {0}".format(i))
+    ret = os.system("diff {0} {1}".format(j, k))
+    if ret:
+        print("\e[31m",i, "not passing\e[0m")
