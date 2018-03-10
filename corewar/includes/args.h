83187224c1e35d8fd0985efd0969d2ecfb0236e9
Alexandre FOURCAT <alexandre.fourcat@epitech.eu>
2018-03-10 05:52:07 +0100

    Adding bite

--- corewar/includes/args.h
+++ corewar/includes/args.h
@@ -9,11 +9,11 @@
 #define ARGS_H_
 
 /*
- * program_t: program information structure
- *
- * live_code: vm_number
- * adress: starting adress
- * filename: PATH to the file
+** program_t: program information structure
+**
+** live_code: vm_number
+** adress: starting adress
+** filename: PATH to the file
 */
 typedef struct program_s {
 	int live_code;
