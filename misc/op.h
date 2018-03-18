0bf5a09e227d47c5056896b4aaec6652da765f5a
Thomas Nicollet <thomas.nicollet@epitech.eu>
2018-03-18 17:10:00 +0100

    Final checking

--- misc/op.h
+++ misc/op.h
@@ -1,95 +1,69 @@
 /*
-** op.h for  korewar
-**
-** Made by Astek
-** Login   <astek@epitech.net>
-**
-** Started on  Mon Mar 30 11:14:31 2009 Astek
-** Last update Tue Mar 22 16:44:20 2011 Astek
+** EPITECH PROJECT, 2017
+** my_printf.c
+** File description:
+** Header for worewar ops.
 */
 
 #ifndef _OP_H_
 #define _OP_H_
 
-#define MEM_SIZE                (6*1024)
-#define IDX_MOD                 512   /* modulo of the index < */
-#define MAX_ARGS_NUMBER         4     /* this may not be changed 2^*IND_SIZE */
+static const int MEM_SIZE = (6*1024);
+static const int IDX_MOD = 512;
+static const int MAX_ARGS_NUMBER = 4;
 
-#define COMMENT_CHAR            '#'
-#define LABEL_CHAR              ':'
-#define DIRECT_CHAR             '%'
-#define SEPARATOR_CHAR          ','
+static const int COMMENT_CHAR = '#';
+static const int LABEL_CHAR = ':';
+static const int DIRECT_CHAR = '%';
+static const int SEPARATOR_CHAR = ',';
 
-#define LABEL_CHARS             "abcdefghijklmnopqrstuvwxyz_0123456789"
+static const char *LABEL_CHARS = "abcdefghijklmnopqrstuvwxyz_0123456789";
 
-#define NAME_CMD_STRING         ".name"
-#define COMMENT_CMD_STRING      ".comment"
+static const char *NAME_CMD_STRING = ".name";
+static const char *COMMENT_CMD_STRING = ".comment";
 
-/*
-** regs
-*/
+static const int REG_NUMBER = 16;
 
-#define REG_NUMBER      16              /* r1 <--> rx */
+typedef char args_type_t;
 
-/*
-**
-*/
+static const int T_REG = 1;
+static const int T_DIR = 2;
+static const int T_IND = 4;
+static const int T_LAB = 8;
 
-typedef char    args_type_t;
-
-#define T_REG           1       /* register */
-#define T_DIR           2       /* direct  (ld  #1,r1  put 1 into r1) */
-#define T_IND           4       /* indirect always relative
-                                   ( ld 1,r1 put what's in the address (1+pc)
-                                   into r1 (4 bytes )) */
-#define T_LAB           8       /* LABEL */
-
-struct  op_s
-{
-   char         *mnemonique;
-   char         nbr_args;
-   args_type_t  type[MAX_ARGS_NUMBER];
-   char         code;
-   int          nbr_cycles;
-   char         *comment;
+struct  op_s {
+	char *mnemonique;
+	char nbr_args;
+	args_type_t type[MAX_ARGS_NUMBER];
+	char code;
+	int nbr_cycles;
+	char *comment;
 };
 
-typedef struct op_s     op_t;
+typedef struct op_s op_t;
 
-/*
-** size (in bytes)
-*/
-#define IND_SIZE        2
-#define DIR_SIZE        4
-#define REG_SIZE        DIR_SIZE
+static const int IND_SIZE = 2;
+static const int DIR_SIZE = 4;
+static const int REG_SIZE = DIR_SIZE;
 
-/*
-** op_tab
-*/
-extern  op_t    op_tab[];
+extern op_t op_tab[];
 
-/*
-** header
-*/
-#define PROG_NAME_LENGTH        128
-#define COMMENT_LENGTH          2048
+static const int PROG_NAME_LENGTH = 128;
+static const int COMMENT_LENGTH = 2048;
+static const int COREWAR_EXEC_MAGIC = 0xea83f3;
 
-struct header_s
-{
-   int  magic;
-#define COREWAR_EXEC_MAGIC      0xea83f3        /* why not */
-   char prog_name[PROG_NAME_LENGTH + 1];
-   int  prog_size;
-   char comment[COMMENT_LENGTH + 1];
+
+struct header_s {
+	int magic;
+	char prog_name[PROG_NAME_LENGTH + 1];
+	int prog_size;
+	char comment[COMMENT_LENGTH + 1];
 };
 
 typedef struct header_s header_t;
 
-/*
-** live
-*/
-#define CYCLE_TO_DIE    1536    /* number of cycle before beig declared dead */
-#define CYCLE_DELTA     5
-#define NBR_LIVE        40
+static const int CYCLE_TO_DIE = 1536;
+static const int CYCLE_DELTA = 5;
+static const int NBR_LIVE = 40;
 
 #endif
